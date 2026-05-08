bb05a4b84bd07c17b427ed697da4a72a9477c0e3
eeempresaaa12-stack <eeempresaaa12@gmail.com>
2026-05-08 14:58:32 -0500

    cuarto commit

--- index.php
+++ index.php
@@ -6,36 +6,35 @@
 <meta charset="UTF-8">
 <title>Control de Producción</title>
 
-<link rel="stylesheet" href="css/estilos.css">
+    <link rel="icon" type="image/png" href="../img/logo-plastypetco2.png">
+    <link rel="stylesheet" href="css/estilos.css">
 
 </head>
 
 <body class="index-body">
 
-<div class="glass-container">
+    <div class="glass-container">
 
-<h1>Control de Producción</h1>
+        <h1>Control de Producción</h1>
 
-<div class="areas">
+        <div class="areas">
 
-<a href="paquetes/dashboard.php" class="area-card">
-<h2>Producción por Paquetes</h2>
-<p>Control de paquetes producidos</p>
-</a>
+        <a href="paquetes/dashboard.php" class="area-card">
+            <h2>Producción por Paquetes</h2>
+            <p>Control de paquetes producidos</p>
+        </a>
+        <a href="rollo/dashboard.php" class="area-card">
+            <h2>Producción por Peso Rollo</h2>
+            <p>Control de rollos producidos</p>
+        </a>
+        <a href="plana/dashboard.php" class="area-card">
+            <h2>Producción Máquina Plana</h2>
+            <p>Control producción máquina plana</p>
+        </a>
 
-<a href="rollo/dashboard.php" class="area-card">
-<h2>Producción por Peso Rollo</h2>
-<p>Control de rollos producidos</p>
-</a>
+        </div>
 
-<a href="plana/dashboard.php" class="area-card">
-<h2>Producción Máquina Plana</h2>
-<p>Control producción máquina plana</p>
-</a>
-
-</div>
-
-</div>
+    </div>
 
 </body>
 </html>
