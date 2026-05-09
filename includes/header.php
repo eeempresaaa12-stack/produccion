432395ab05b5af3aea874683041ac2c04df5e013
eeempresaaa12-stack <eeempresaaa12@gmail.com>
2026-05-09 11:57:01 -0500

    septimo commit

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
 
-<meta charset="UTF-8">
+<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Control Producción</title>
 
     <link rel="icon" type="image/png" href="../img/logo-plastypetco.png">
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
 
-<meta charset="UTF-8">
+<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Control de Producción</title>
 
     <link rel="icon" type="image/png" href="./img/logo-plastypetco.png">
