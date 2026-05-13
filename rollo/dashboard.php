68aec51468c69bfca11850a53eb0c00592da4d80
eeempresaaa12-stack <eeempresaaa12@gmail.com>
2026-05-13 16:22:50 -0500

    produccion 1.1

--- rollo/dashboard.php
+++ rollo/dashboard.php
@@ -1,4 +1,11 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+
+<?php
 /** @var mysqli $conexion */
 
 include("../includes/header.php");
