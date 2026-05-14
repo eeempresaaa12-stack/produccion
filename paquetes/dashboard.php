12fd31ff4ced2f0f825b70f59952171e89bc3f01
eeempresaaa12-stack <eeempresaaa12@gmail.com>
2026-05-14 14:06:09 -0500

    produccion 1.22

--- paquetes/dashboard.php
+++ paquetes/dashboard.php
@@ -2,7 +2,7 @@
 /** @var mysqli $conexion */
 
 include("../includes/header.php");
-require_once($_SERVER['DOCUMENT_ROOT'] . '/conexion.php'); 
+require_once("../conexion.php");
 
 /* ACTIVAR ERRORES (solo una vez) */
 
